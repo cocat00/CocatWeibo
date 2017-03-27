b9668e8089a0f3c67e1e8ba544660cf5cd9b8bdc
cocat00 <chenjiesjj@126.com>
2017-03-27 14:36:04 +0800

    网络库添加put方法

--- CocatWeibo/Classes/Other/Tool/CJHttpTool.h
+++ CocatWeibo/Classes/Other/Tool/CJHttpTool.h
@@ -30,4 +30,14 @@
  */
 + (void)postWithURL:(NSString *)url params:(NSDictionary *)params success:(void (^)(id json))success failure:(void (^)(NSError *error))failure;
 
+/**
+ *  发送一个PUT请求
+ *
+ *  @param url     请求路径
+ *  @param params  请求参数
+ *  @param success 请求成功后的回调
+ *  @param failure 请求失败后的回调
+ */
++ (void)putWithURL:(NSString *)url params:(NSDictionary *)params success:(void (^)(id json))success failure:(void (^)(NSError *error))failure;
+
 @end
--- CocatWeibo/Classes/Other/Tool/CJHttpTool.m
+++ CocatWeibo/Classes/Other/Tool/CJHttpTool.m
@@ -53,4 +53,27 @@
       }];
 }
 
++ (void)putWithURL:(NSString *)url params:(NSDictionary *)params success:(void (^)(id json))success failure:(void (^)(NSError *error))failure
+{
+    // 1.创建请求管理对象
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    
+    // 说明服务器返回的JSON数据
+    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html", @"application/x-gzip", @"text/plain", nil];
+    
+    // 2.发送请求
+    [mgr PUT:url parameters:params
+      success:^(AFHTTPRequestOperation *operation, id responseObject) {
+          if (success) {
+              success(responseObject);
+          }
+      } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+          if (failure) {
+              failure(error);
+          }
+      }];
+}
+
 @end
